69a62a56e9831ebc96076a8505a00b2a86c4675c
Luis Pedro Coelho <luis@luispedro.org>
2018-04-20 11:23:24 +0200

    BLD Pin nixpkg version that works well

--- default.nix
+++ default.nix
@@ -1,23 +1,30 @@
-with import <nixpkgs> {};
-
 let
+pkgs = let
+  hostPkgs = import <nixpkgs> {};
+  pinnedPkgs = hostPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "00335098a5cde0b221db83e555019a251d3d40f9";
+    sha256 = "0nsrp0ardrimac64qsqy5rymvychzzkwripgrw8g9gvfa5kgv3xr";
+  };
+in import pinnedPkgs {};
 
 envname = "luispedro_org";
-jenv = bundlerEnv {
+jenv = pkgs.bundlerEnv {
   name = "jekyll_rbst_env";
   gemset = ./gemset.nix;
   gemfile = ./Gemfile;
   lockfile = ./Gemfile.lock;
  };
 
-py27 = python27.withPackages (pp: [
+py27 = pkgs.python27.withPackages (pp: [
       pp.docutils
       pp.pygments
       pp.awscli
       ]);
 
 in
-stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
   name = "luispedro_org";
 
   buildInputs = with pkgs; [
