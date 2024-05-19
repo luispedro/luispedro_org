df4cb41fac9882feedf81a168763947664ac7e30
Luis Pedro Coelho <luis@luispedro.org>
2024-05-19 21:40:45 +1000

    MIN Mention SF trip

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -111,12 +111,12 @@ My [Erdös-Bacon number is 7](/erdos-bacon).
 # Upcoming Travels & Talks
 
 - **Aug 17-22, 2025** [Decoding Microproteins Across Evolution and Disease GRC](https://www.grc.org/decoding-microproteins-across-evolution-and-disease-conference/2025/) in Barcelona, Spain
-- **Nov 11-15** I  will be co-teaching a 1 week course on state-of-the-art bioinformatic approaches to analyze metagenomic data. Click [here](https://www.physalia-courses.org/courses-workshops/environmental-metagenomics/) to learn more and register.
+- **Nov 11-15** I will be co-teaching a 1 week course on state-of-the-art bioinformatic approaches to analyze metagenomic data. Click [here](https://www.physalia-courses.org/courses-workshops/environmental-metagenomics/) to learn more and register.
+- **July 19-24**: I will be in San Francisco
 - **July 12-17**: [ISMB2024, followed by the Quest for Orthologs meeting](https://www.iscb.org/ismb2024/) in Montreal, Canada
 - **July 4**: [ASM (the Australian one)](https://asmicro-2024.p.asnevents.com.au/days/2024-07-04), in Brisbane, QLD, Australia
 
-[Email me](mailto:luis@luispedro.org) if you want to set up meetings at
-any of these opportunities (or to invite me for other opportunities).
+[Email me](mailto:luis@luispedro.org) if you want to set up meetings at any of these opportunities (or to invite me for other opportunities).
 
 If you want to chat with me about science and such, you can use [my cal.com
 link](https://cal.com/luis-pedro-coelho/zoom-15).
